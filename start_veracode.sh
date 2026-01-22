edbcbb3cee0bf39acbea3e2237eb31e26c0bddfe
Zane Leonard <74984168+zane1218@users.noreply.github.com>
2026-01-22 13:52:43 -0600

    Update start_veracode.sh

--- start_veracode.sh
+++ start_veracode.sh
@@ -5,16 +5,14 @@
 # Stop the script as soon as the first command fails
 set -euo pipefail
 
-#check for environment variables
-: "$VERACODE_WEBHOOK"
-: "$VERACODE_SECRET_ID" 
-: "$VERACODE_SECRET_ID_KEY"
+# Set WEBHOOK to webhook secret (without URL)
+WEBHOOK=$1
 
 # Set the Veracode API ID
-API_ID=$(echo $VERACODE_SECRET_ID | cut -d "-" -f 2)
+API_ID=$(echo $2 | cut -d "-" -f 2)
 
 # Set the Veracode API SECRET
-API_SECRET=$(echo $VERACODE_SECRET_ID_KEY | cut -d "-" -f 2)
+API_SECRET=$(echo $3 | cut -d "-" -f 2)
 
 # Set the API endpoint
 API_ENDPOINT="api.veracode.com"
@@ -23,15 +21,15 @@ API_PATH="/dae/api/core-api/webhook"
  generate_hmac_header() { 
  VERACODE_AUTH_SCHEMA="VERACODE-HMAC-SHA-256"
  VERACODE_API_VERSION="vcode_request_version_1"
- signing_data=$VERACODE_WEBHOOK
+ signing_data=$1
 
  nonce="$(openssl rand -hex 16)"
  timestamp=$(date +%s"000")
 
- nonce_key=$(echo "$nonce" | xxd -r -p | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$API_SECRET" | awk -F" " '{ print $VERACODE_SECRET_ID }')
- time_key=$(echo -n "$timestamp" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$nonce_key" | awk -F" " '{ print $VERACODE_SECRET_ID }')
- sig_key=$(echo -n "$VERACODE_API_VERSION" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$time_key" | awk -F" " '{ print $VERACODE_SECRET_ID }')
- signature=$(echo -n "$signing_data" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$sig_key" | awk -F" " '{ print $VERACODE_SECRET_ID }')
+ nonce_key=$(echo "$nonce" | xxd -r -p | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$API_SECRET" | awk -F" " '{ print $2 }')
+ time_key=$(echo -n "$timestamp" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$nonce_key" | awk -F" " '{ print $2 }')
+ sig_key=$(echo -n "$VERACODE_API_VERSION" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$time_key" | awk -F" " '{ print $2 }')
+ signature=$(echo -n "$signing_data" | openssl dgst -sha256 -mac HMAC -macopt hexkey:"$sig_key" | awk -F" " '{ print $2 }')
 
 echo "$VERACODE_AUTH_SCHEMA id=$API_ID,ts=$timestamp,nonce=$nonce,sig=$signature"
  } 
@@ -80,7 +78,7 @@ echo "Scan finished with status $STATUS."
 
 #### Download Scan Report ####
 
-#signing_data="id=$API_ID&host=$API_ENDPOINT&url=$API_PATH/$WEBHOOK/scans/$SCAN_ID/report/junit&method=GET"
+###signing_data="id=$API_ID&host=$API_ENDPOINT&url=$API_PATH/$WEBHOOK/scans/$SCAN_ID/report/junit&method=GET"
 
 #VERACODE_AUTH_HEADER=$(generate_hmac_header $signing_data)
 
